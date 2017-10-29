53b9e90a36acf582d15a8397a702c11fb80b69a8
adrielbarrettjo <a.barrettjohnson@gmail.com>
2017-10-29 14:17:38 -0400

    got the initial API calls working

--- main.js
+++ main.js
@@ -1,29 +1,64 @@
-function makeRequest(code) {
-	var countrylong = isoConvert(code);
-	console.log(countrylong);
-	return $.ajax({ 
-		url: 'http://api.worldbank.org/countries',
-		type: 'GET',
+function getPovertySeverity(code) {
+	console.log(code);
+	let povertySeverity = $.ajax({ 
+		//http://api.worldbank.org/countries/az/indicators/1.2.PSev.2.5usd?format=jsonP&prefix=Getdata
+		url: 'http://api.worldbank.org/countries/'+code+'/indicators/SP.POP.TOTL',
 		data: {
-			countries: countrylong,
-			indicators: "3.0.Gini",
-			format: JSON
-		}
+			format: 'jsonP',
+			date: '2000:2017'
+		},
+		type: 'GET',
+		dataType: 'jsonp',
+		jsonp: 'prefix',
 	});
+	console.log(povertySeverity)
+	return povertySeverity;
 }
 
-// function handleData(data) {
-// 	console.log(data);
-// }
+function getCPI(code) {
+
+	let CPI = $.ajax({ 
+		url: 'http://api.worldbank.org/countries/'+code+'/indicators/2.01.03.01.prcpbase',
+		type: 'GET',
+		data: {
+			format: 'jsonP',
+			date: '2000:2017'
+		},
+		dataType: 'jsonp',
+		jsonp: 'prefix'
+	});
+	console.log(CPI)
+	return CPI;
+
+}
+
+function getGiniCoefficient() {
+	console.log('gini ran');
+	let giniCoefficient = '1';
+	return giniCoefficient;
+}
+
+function getNetIncome() {
+	console.log('netIncome ran');
+	let netIncome = '1';
+	return netIncome;
+}
 
 
-function generateCountryText() {
+function generateCountryText(data) {	
+	const povertySeverity = data[0][1][0].value;
+	console.log(povertySeverity);
+	const CPI = data[1][1][0].value;
+	console.log(CPI);
+	let giniCoefficient = data[2];
+	let netIncome = data[3];
+	console.log(data)
 	let indicatorText = (`
 	<div class="indicators">
-		<p>Poverty Severity</p>
-		<p>Consumer Price Index</p>
-		<p>Gini Coefficient</p>
-		<p>Net Income as % of GDP</p>
+		<p>Poverty Severity: ${povertySeverity}   </p>
+		<p>Consumer Price Index: ${CPI} </p>
+		<p>Gini Coefficient: ${giniCoefficient} </p>
+		<p>Net Income as % of GDP: ${netIncome} </p>
 	</div>
 
 	<input type="submit" role="button" class="back-button" alt="Go Back to Map" value="Go Back to Map">
@@ -32,16 +67,21 @@ function generateCountryText() {
 }
 
 function clickCountry(event, code, region) {
+	let povertySeverity = getPovertySeverity(code);
+	let CPI = getCPI(code);
+	let giniCoefficient = getGiniCoefficient(code);
+	let netIncome = getNetIncome(code);
+	Promise.all([povertySeverity, CPI, giniCoefficient, netIncome])
+		.then(function(data) {
+			$('.country-page').html(generateCountryText(data));
+		});
 	console.log(event);
 	console.log(code);
 	console.log(region);
-	makeRequest(code);
-	console.log(typeof(code))
-	// makeRequest(code).then(handleData);
-
-
-
-	$('.country-page').html(generateCountryText());
+	// generateCountryText();
+	// getPovertySeverity(code).then(handleData);
+	console.log(typeof(code));
+	// $('.country-page').html(generateCountryText());
 	//once data is appended to the dom, remove hidden class
 	$('.map').addClass('hidden');
 	$('.subheader').addClass('hidden');
